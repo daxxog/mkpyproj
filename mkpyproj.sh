d79070b69966a5a31bc72f264bd9b857ce6ba393
Volm, David <david.volm@wildcardcorp.com>
2020-12-18 13:49:09 -0600

    basic script

--- mkpyproj.sh
+++ mkpyproj.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+SOURCE_GH_REPO="daxxog/envsubst-mustache"
+AT_COMMIT="caaf2b3ee50e7e1d64a6dfca0b3ef44473c24437"
+PULL_FROM="https://raw.githubusercontent.com/${SOURCE_GH_REPO}/${AT_COMMIT}"
+
+try_pull_remote() {
+    if [ ! -f ${1} ]; then
+        wget "${PULL_FROM}/${1}"
+    else
+        echo "file '${1}' already exists !"
+        exit 1
+    fi
+}
+
+echo "pulling template -->\n" && \
+    try_pull_remote "env.sh" && \
+    try_pull_remote "requirements.dev.txt" && \
+    try_pull_remote ".gitignore" && \
+    try_pull_remote ".python-version" && \
+    echo "template pulled !!"
